727625ef25c0d737e2177b4288d53bed2137fadd
Yurii <y.pakhariev@mobilunity.com>
2018-11-19 18:16:24 +0200

    updated plugin

---
+++ www/js/s2c.js
@@ -0,0 +1,25 @@
+const block = document.getElementById('s2ccalls');
+// Put your own token
+const token = '';
+
+// Put your user id
+const userId = '';
+
+// Put your login identifier
+const loginIdentifier = '';
+
+// Change for dev/prod mode
+const isDebug = true;
+
+block.addEventListener('click', () => {
+  if (cordova && cordova.plugins && cordova.plugins.S2CPlugin) {
+    console.log('can start IDnow');
+    cordova.plugins.S2CPlugin.getUserInfo(
+        token,
+        userId,
+        loginIdentifier,
+        isDebug,
+        (response) => console.log('success', response),
+        (message) => console.log('failure', message));
+  }
+});
